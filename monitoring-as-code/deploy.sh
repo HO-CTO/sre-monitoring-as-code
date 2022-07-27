e9d33049acc31e9335fecdf7a4f2574b28c0c307
Michael Pearson <michael.pearson@digital.homeoffice.gov.uk>
2022-07-27 12:02:52 +0100

    refactor: rename docker image name

--- monitoring-as-code/deploy.sh
+++ monitoring-as-code/deploy.sh
@@ -5,6 +5,6 @@
 rm -rf "$PWD"/output/*/
 
 # Executes docker image to create rules and dashboards for monitoring and summary mixin files
-docker run --mount type=bind,source="$PWD"/output,target=/output --mount type=bind,source="$PWD"/mixin-defs,target=/input -it mac:latest -m monitoring -rd -i input -o output
+docker run --mount type=bind,source="$PWD"/output,target=/output --mount type=bind,source="$PWD"/mixin-defs,target=/input -it sre-monitoring-as-code:latest -m monitoring -rd -i input -o output
 
-docker run --mount type=bind,source="$PWD"/output,target=/output --mount type=bind,source="$PWD"/mixin-defs,target=/input -it mac:latest -m summary -d -i input -o output
+docker run --mount type=bind,source="$PWD"/output,target=/output --mount type=bind,source="$PWD"/mixin-defs,target=/input -it sre-monitoring-as-code:latest -m summary -d -i input -o output
