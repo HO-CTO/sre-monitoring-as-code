a56a05ae0df54d4d3545179e1037351168aaa4d9
Mahruf Iqbal <102766665+mahrufiqbalHO@users.noreply.github.com>
2022-09-30 09:34:48 +0100

    refactor: update workflow to use jsonnet fmt/lint container image (#263)
    * refactor: update workflow to use jsonnet fmt/lint container image
    
    * refactor: add fmt/lint version

--- monitoring-as-code/fmt.sh
+++ monitoring-as-code/fmt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-echo "find . -name 'pkg' -prune -o -name 'vendor' -prune -o -name '*.libsonnet' -print -o -name '*.jsonnet' -print | xargs -n 1 jsonnetfmt -n 2 --max-blank-lines 2 --string-style s --comment-style s --no-sort-imports -i" | docker run -i -v "$(pwd):/app/MaC" jsonnet-dev-tool /bin/sh
+echo "jsonnetfmt -v && find . -name 'pkg' -prune -o -name 'vendor' -prune -o -name '*.libsonnet' -print -o -name '*.jsonnet' -print | xargs -n 1 jsonnetfmt -n 2 --max-blank-lines 2 --string-style s --comment-style s --no-sort-imports -i" | docker run -i -v "$(pwd):/app/MaC" ghcr.io/ho-cto/sre-fmt-lint:latest /bin/sh
