dacbbe5f73b2a4bf5f499f53953f9b905fb9cd4a
Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>
2022-11-18 14:43:57 +0000

    Add main loop.

--- example-apps/sre-metric-seeder/src/index.ts
+++ example-apps/sre-metric-seeder/src/index.ts
@@ -1,17 +1,14 @@
+import { skipPartiallyEmittedExpressions } from 'typescript';
 import main from './invoke/invoker'
 
-/*
+const sleep = async (durationInMillis: number) => new Promise(f => setTimeout(f, durationInMillis));
 
-for loop
-    amazon sqs invoker
-    amzaon rds invoker
-    amazon etc.. envoker
+(async () => {
 
-    https node/java enoker
+    let count = 0;
+    while(true) {
+        console.log(`Running ${count++}`);
+        await sleep(500);
+    }
 
-    sleep 10 s
-
-*/
-
-while true:
-    main()
+})();
