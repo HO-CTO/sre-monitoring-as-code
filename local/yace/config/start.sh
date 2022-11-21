e3b89fd93ce9d2baf3abba3a16f985424ca4f76c
Michael Pearson <michael.pearson@digital.homeoffice.gov.uk>
2022-11-21 08:09:12 +0000

    refactor: split yace static and discovery config

--- local/yace/config/start.sh
+++ local/yace/config/start.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-# env var replacement with comma delimiter to avoid conflict with url strings
-sed "s/@@YACE_ENVIRONMENT_TAG@@/${YACE_ENVIRONMENT_TAG}/g" "/config/config.yml" > "/config/newconfig.yml"
+# YACE Job Type variable assignment
+JOB=$YACE_JOB_TYPE
+
+if [ "$JOB" = "dis" ]; then
+  awk '
+      FNR==1 && NR!=1 { while (/^static:/) getline; }
+      1 {print}
+  ' /config/static/*.yml > /config/config.yml
+  # sed '1d' /config/static/*.yml  > /config/config.yml
+elif [ "$JOB" = "discovery" ]; then
+  # env var replacement with comma delimiter to avoid conflict with url strings
+  sed "s/@@YACE_ENVIRONMENT_TAG@@/${YACE_ENVIRONMENT_TAG}/g" "/config/discovery/config.yml" > "/config/config.yml"
+else
+  echo "no yace config file has been supplied"
+  exit
+fi
 
 # Invoke yace config and set flags
 exec yace \
---config.file=/config/newconfig.yml \
+--config.file=/config/config.yml \
 --scraping-interval=60 \
 --debug=false
