69ae4a460027092b0263004db03d74a7323969e6
Michael Pearson <michael.pearson@digital.homeoffice.gov.uk>
2022-11-21 08:41:06 +0000

    refactor: add comments to yace entrypoint

--- local/yace/config/start.sh
+++ local/yace/config/start.sh
@@ -3,16 +3,19 @@
 # YACE Job Type variable assignment
 JOB=$YACE_JOB_TYPE
 
-if [ "$JOB" = "dis" ]; then
+# Conditional to check for static or auto-discovery job type in global environment variables
+if [ "$JOB" = "static" ]; then
+  # if static merge files and remove all first lines from all but first file
   awk '
       FNR==1 && NR!=1 { while (/^static:/) getline; }
       1 {print}
   ' /config/static/*.yml > /config/config.yml
-  # sed '1d' /config/static/*.yml  > /config/config.yml
+
 elif [ "$JOB" = "discovery" ]; then
-  # env var replacement with comma delimiter to avoid conflict with url strings
+  # if discovery, env var replacement with comma delimiter to avoid conflict with url strings
   sed "s/@@YACE_ENVIRONMENT_TAG@@/${YACE_ENVIRONMENT_TAG}/g" "/config/discovery/config.yml" > "/config/config.yml"
 else
+  # else no yace job type set exit with error prompt
   echo "no yace config file has been supplied"
   exit
 fi
